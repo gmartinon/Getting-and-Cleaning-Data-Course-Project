bee15d2c10f7033b58c76f75a2d9067e1c93de1e
gmartinon <gregoire.martinon@gmail.com>
2017-09-10 21:26:51 +0200

    updates

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# Confession
-what should be in the README.txt file since everything is in the CodeBook?
+## What run_analysis.R does (see also the comments in run_analysis.R)
+
+In order for the script to work properly, the directory UCI_HAR_Dataset downloaded should be in your working directory.
+
+* STEP 1 : we first merge the test and train data sets using a simple rbind.
+* STEP 2 : we extract columns names from the features.txt file. We then retain only the columns whose names contains mean() or
+  std(). This is done with help the grep command.
+* STEP 3 : we supplement the data with two columns (two calls to mutate), the subject id column and the activity column, retrieved
+  from the subject.txt and y.txt files. Again we have to merge train and test data set with calls to rbind in the same order as
+  before. This ensures that the subject and activity column match well the measurement order.
+* STEP 4 : we give more explicit names to the columns, by expandig the abbreviations. This is done with the gsub function.
+* STEP 5 : we group the data set by subject AND activity, with a call to the group_by (dplyr) function with two arguments. The summarise_all
+  function (with a function mean argument) of the dplyr package then collapses the information of each variable for each pair
+  (subject, activity) by taking the average in each corresponding group.
+
+The resulting data set called tidydataset, gives for each variable and each combination, the mean of the variable. For example, the very first
+numerical value is 0.22159824394 and is the averaged measurement of tBodyAcc-mean-X for subject number 1 when laying.
+
+Since there are 30 subjects and 6 activities, the file is only 30*6 = 180 lines long
--- run_analysis.R
+++ run_analysis.R
@@ -1,49 +1,41 @@
-### step 1 :  merge test and training sets
-dt <- rbind(read.table("./UCI_HAR_Dataset/test/X_test.txt"),
-            read.table("./UCI_HAR_Dataset/train/X_train.txt"))
-
-### step 2 and 4: name the columns and extract the ones featuring a mean() or std()
-colnames(dt) <- read.table("./UCI_HAR_Dataset/features.txt",
-                           stringsAsFactors = FALSE)[,2]
-dt <- dt[, grep('mean\\(\\)|std\\(\\)', colnames(dt), value = TRUE)]
-
-### step 3 : add columns with descriptive activity names and subject id
-# descriptive activity name
-act <- rbind(read.table("./UCI_HAR_Dataset/test/y_test.txt"),
-             read.table("./UCI_HAR_Dataset/train/y_train.txt"))
-act <- read.table("./UCI_HAR_Dataset/activity_labels.txt",
-                  stringsAsFactors = FALSE)[act[,1],2]
-dt[["activity"]] <- act
-# subject id
-sub <- rbind(read.table("./UCI_HAR_Dataset/test/subject_test.txt"),
-             read.table("./UCI_HAR_Dataset/train/subject_train.txt"))
-dt[["subject"]] <- sub[,1]
-
-### step 5 : tidy data set with average of each variable for each subject and activity
-res <- aggregate(. ~ activity + subject, dt, mean)
-# put subject column at first position
-res <- res[,c(2,1,3:ncol(res))]
-# embrace colnames with <> to denote that the value is averaged
-colnames(res)[3:ncol(res)] <- paste("<", colnames(res)[3:ncol(res)], ">", sep = "")
-
-write.table(res, "tidydata.txt", row.names = FALSE)
-
-
-
-
-
-
-
-
-### bonus : the dplyr version
 library(dplyr)
-res <- dt %>% group_by(subject, activity) %>% summarise_all(mean)
-colnames(res)[3:ncol(res)] <- paste("<", colnames(res)[3:ncol(res)], ">", sep = "")
 
-### extra bonus : narrow or wide representations
-### I think the wide representation better matches the tidy data set definition
-library(tidyr)
-# going from wide to narrow table
-res <- res %>% gather(average, value, -(subject:activity)) %>% arrange(subject)
-# going from narrow to wide table
-res <- res %>% spread(average, value)
+### step 1
+testdataset <- read.table("./UCI_HAR_Dataset/test/X_test.txt")                   # read test data set
+traindataset <- read.table("./UCI_HAR_Dataset/train/X_train.txt")                # read train data set
+dataset <- rbind(testdataset, traindataset)                                      # merge into full data set
+
+### step 2
+features <- read.table("./UCI_HAR_Dataset/features.txt")                         # read the features
+colnames(dataset) <- features[,2]                                                # rename the columns of the full data set according to the features
+mycolnames <- grep('mean\\(\\)|std\\(\\)', colnames(dataset), value = TRUE)      # find column names evoking a mean of standard deviation
+dataset <- dataset[, mycolnames]                                                 # keep only these columns in the data set
+
+### step 3
+# descriptive activity name
+testactivities <- read.table("./UCI_HAR_Dataset/test/y_test.txt")                # read test activities
+trainactivities <- read.table("./UCI_HAR_Dataset/train/y_train.txt")             # read train activities
+activities <- rbind(testactivities, trainactivities)                             # merge the activities (in the same order as in step 1)
+activityindices <- activities[,1]                                                # just the first (and single) column, put as a vector
+activitylabels <- read.table("./UCI_HAR_Dataset/activity_labels.txt")            # read the activity labels
+dataset <- mutate(dataset, activity = activitylabels[activityindices,2])         # add the column activity to the data set, with name from the labels
+# subject id
+testids <- read.table("./UCI_HAR_Dataset/test/subject_test.txt")                 # read test ids
+trainids <- read.table("./UCI_HAR_Dataset/train/subject_train.txt")              # read train ids
+ids <- rbind(testids, trainids)                                                  # merge the ids (in the same order as in step 1)
+dataset <- mutate(dataset, subject = ids[,1])                                    # add the column subject to the data set, filled with the ids
+
+### step 4
+newcolnames <- colnames(dataset)                                                 # perform some string replacements with gsub function
+newcolnames <- gsub("^t", "timeDomain.", newcolnames)                            # new names are more explicit
+newcolnames <- gsub("^f", "frequencyDomain.", newcolnames)
+newcolnames <- gsub("Acc", "Acceleration", newcolnames)
+newcolnames <- gsub("Gyro", "Gyroscope", newcolnames)
+newcolnames <- gsub("Mag", "Magnitude", newcolnames)
+newcolnames <- gsub("\\(\\)", "", newcolnames)
+colnames(dataset) <- newcolnames
+
+### step 5
+tidydataset <- dataset %>% group_by(subject, activity) %>%                       # group the data set according to the subject id and activity name
+                           summarise_all(mean)                                   # summarise the data for each pair of (subject, activity) by collapsing the data with the mean function
+write.table(tidydataset, "tidydata.txt", row.names = FALSE)                      # write the result into the text file
